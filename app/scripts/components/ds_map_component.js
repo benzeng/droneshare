4a30ef5296fa7790a039c2205000751d2e717d9d
Ramón Roche <mrpollo@gmail.com>
2015-01-09 01:17:53 -0800

    show mapbox map on index
    this is a get quick revision that needs lots of improvements:
    - set dynamic id name of component div (so we can recycle the component)
    - make sure the resizing handlers know the context in which they have
      been called
    - find a way to store globals for the mapbox access tokens and layers
    - cleanup the component js, its ugly and you should feel bad about it!

---
+++ app/scripts/components/ds_map_component.js
@@ -0,0 +1,23 @@
+Droneshare.DsMapComponent = Ember.Component.extend({
+  tagName: 'div',
+  attributeBindings: ['customId:id'],
+  customId: 'map-component',
+
+  didInsertElement: function() {
+    //debugger;
+    var resizeMapHandler = function() {
+      $('#map-component').css({
+        height: parseInt(window.innerHeight, 10) + "px"
+      });
+    };
+    $(window).bind('resize', function(event) {
+      resizeMapHandler();
+    });
+    resizeMapHandler()
+    L.mapbox.accessToken = 'pk.eyJ1IjoibXJwb2xsbyIsImEiOiJtUG0tRk9BIn0.AqAiefUV9fFYRo-w0jFR1Q';
+    L.mapbox.map('map-component', 'mrpollo.kfbnjbl0');
+    //threedr_default: mbox("kevin3dr.hokdl9ko", "Topographic")
+    //threedr_satview: mbox("kevin3dr.io0162i9", "Satellite")
+    //airspace_warning: mbox("mslee.h1kk2o6r", "Restricted Zones", '<a href="http://openstreetmap.org">NPS</a>, <a href="https://explore.data.gov/National-Security-and-Veterans-Affairs/Military-Installations-Ranges-and-Training-Areas/wcc7-57p3">US Military Data</a>')
+  }
+});
