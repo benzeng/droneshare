116e5f1b85623e6928570cd68632c399b7518c9d
Ramón Roche <mrpollo@gmail.com>
2015-01-22 12:30:16 -0800

    move staticMap internals to mission model utils

--- app/scripts/models/mission_model.js
+++ app/scripts/models/mission_model.js
@@ -1,5 +1,10 @@
 Droneshare.MissionSerializer = DS.JSONSerializer.extend({
   extractArray: function(store, type, arrayPayload, id, requestType) {
+    if (arrayPayload.updates) {
+      arrayPayload = arrayPayload.updates.map(function(item) {
+        return item.payload;
+      });
+    }
     arrayPayload = arrayPayload.filter(function(item) {
       // reject invalid missions
       if (!item.latitude) {
@@ -16,6 +21,17 @@ Droneshare.MissionAdapter = DS.RESTAdapter.extend({
     var camelized = Ember.String.camelize(type);
     return camelized;
   },
+
+  findLive: function(store, type, sinceToken) {
+    var query, host = this.get('host'),
+    namespace = this.get('namespace');
+
+    if (sinceToken) {
+      query = { since: sinceToken };
+    }
+
+    return this.ajax(host + '/' + namespace + '/mission/staticMap', 'GET', { data: query });
+  }
 });
 
 Droneshare.Mission = DS.Model.extend({
@@ -41,40 +57,3 @@ Droneshare.Mission = DS.Model.extend({
   numParameters: DS.attr('number'),
   vehicleType: DS.attr()
 });
-
-Droneshare.IndexSerializer = DS.JSONSerializer.extend({
-  extractArray: function(store, type, arrayPayload, id, requestType) {
-    arrayPayload = arrayPayload.updates.map(function(item) {
-      return item.payload;
-    });
-    arrayPayload = arrayPayload.filter(function(item) {
-      // reject invalid missions
-      if (!item.latitude) {
-        return false;
-      }
-      return true;
-    });
-    return this._super(store, type, arrayPayload, id, requestType)
-  }
-});
-
-Droneshare.IndexAdapter = DS.RESTAdapter.extend({
-  findAll: function(store, type, sinceToken) {
-    var query;
-
-    if (sinceToken) {
-      query = { since: sinceToken };
-    }
-
-    return this.ajax(this.buildURL(type.typeKey), 'GET', { data: query });
-  },
-  buildURL: function(type, id, record) {
-    var host = this.get('host'),
-    namespace = this.get('namespace');
-    return host + '/' + namespace + '/mission/staticMap';
-  }
-});
-
-Droneshare.Index = Droneshare.Mission.extend({
-});
-
